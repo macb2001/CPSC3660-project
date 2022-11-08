a912c5a6f467fc050812e4cbba4354f2f0ba698c
brenthawkings <brenthawk17@gmail.com>
2022-11-08 15:01:05 -0700

    Main Page update

--- project/main.php
+++ project/main.php
@@ -2,12 +2,11 @@
 <h1> Fake Street Realty </h1>
 <p>Select from one of the following operations:</p>
 <ul>
-<li><a href="insert_agent.php">Insert a new Agent</a></li>
-<li><a href="select_agent.php">list Agents</a></li> 
-<li><a href="insert_client.php">Insert a Client</a></li>
-<li><a href="select_client.php">list Clients</a></li> 
+<li><a href="insert_person.php">Insert a new Client or Agent</a></li>
+<li><a href="select_agent.php">list Agents</a></li>
+<li><a href="select_client.php">list Clients</a></li>
 <li><a href="insert_property.php">Insert a Property</a></li>
-<li><a href="select_property.php">List all Propertys</a></li> 
+<li><a href="select_property.php">List all Properties</a></li>
 </ul>
 <p><a href="logout.php">Logout</a></p>
 </html>
