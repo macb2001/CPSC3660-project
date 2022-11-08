ed600e57f4bbbe6694f9af2a42e3054efc76b893
Justin <alhoj@uleth.ca>
2022-11-08 15:28:36 -0700

    Updated select_agent.php

--- project/select_agent.php
+++ project/select_agent.php
@@ -1,6 +1,57 @@
 <html>
-	<head><title>Fake Street Realty</title></head>
-	<body>
+<body>
+  <?php
+  if (isset($_COOKIE["username"])) {
+    $username = $_COOKIE["username"];
+    $password = $_COOKIE["password"];
 
-	</body>
+    $conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,$username);
+    if($mysqli->connect_errno) {
+      echo "Connection Issue!";
+      exit;
+    }
+
+    $sql = "SELECT a_id, name, dob, address, phone, commission FROM AGENT, PERSON
+      WHERE a_id = id;"
+    if($conn->query($sql)) {
+	     echo "
+        <table border='1'>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Date of Birth</th>
+            <th>Address</th>
+            <th>Phone Number</th>
+          </tr>
+       ";
+       foreach ($row as $data) {
+         echo "
+          <tr>
+            <td>{$data['id']}</td>
+            <td>{$data['name']}</td>
+            <td>{$data['dob']}</td>
+            <td>{$data['address']}</td>
+            <td>{$data['phone']}</td>
+            <td>{$data['commission']}</td>
+            <td><a href='update_agent.php?id={$data['a_id']}'>Update</a></td>
+            <td><a href='delete_agent.php?id={$data['a_id']}'>Delete</a></td>
+          </tr>
+         ";
+       }
+       echo "</table><br /><br />\n";
+       echo "<a href='main.php'>Back</a>"
+     } else {
+       $err = $conn->errno;
+       echo "<p>MySQL error code $err </p>";
+     }
+  } else {
+    echo "<h3>You are not logged in!</h3><p> <a href=\"index.php\">Login First</a></p>";
+  }
+
+  if(isset($conn)) {
+    $conn = null;
+  }
+?>
+</body>
+
 </html>
