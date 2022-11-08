76346867af7c9aa07f6b5985fec443b478d02462
Justin <alhoj@uleth.ca>
2022-11-08 16:02:20 -0700

    Updated select_client.php

--- project/select_client.php
+++ project/select_client.php
@@ -1,6 +1,66 @@
 <html>
-	<head><title>Fake Street Realty</title></head>
-	<body>
+<head>
+  <title>View Clients</title>
+</head>
+<body>
+  <?php
 
-	</body>
+  if (isset($_COOKIE["username"])) {
+    $username = $_COOKIE["username"];
+    $password = $_COOKIE["password"];
+
+    $conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,$username);
+    if($mysqli->connect_errno) {
+      echo "Connection Issue!";
+      exit;
+    }
+
+    $sql = "SELECT c_id, name, dob, address, phone FROM CLIENT, PERSON
+      WHERE c_id = id;";
+
+    if($conn->query($sql)) {
+      $result = $conn->query($sql);
+	     echo "
+        <table border='1'>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Date of Birth</th>
+            <th>Address</th>
+            <th>Phone Number</th>
+          </tr>
+       ";
+
+       foreach ($result as $data) {
+         echo "
+          <tr>
+            <td>{$data['c_id']}</td>
+            <td>{$data['name']}</td>
+            <td>{$data['dob']}</td>
+            <td>{$data['address']}</td>
+            <td>{$data['phone']}</td>
+            <td><a href='update_client.php?id={$data['a_id']}'>Update</a></td>
+            <td><a href='delete_client.php?id={$data['a_id']}'>Delete</a></td>
+          </tr>
+         ";
+       }
+
+       echo "</table><br /><br />\n";
+       echo "<a href='main.php'>Back</a>";
+
+     } else {
+       $err = $conn->errno;
+       echo "<p>MySQL error code $err </p>";
+     }
+
+  } else {
+    echo "<h3>You are not logged in!</h3><p> <a href=\"index.php\">Login First</a></p>";
+  }
+
+  if(isset($conn)) {
+    $conn = null;
+  }
+  ?>
+</body>
+
 </html>
