1b153640280b1f836c87353107b461ed0a762d09
Mackenzie Broughton <macwbroughton@gmail.com>
2022-11-08 14:38:55 -0700

    insert_agent update

--- project/insert_agent.php
+++ project/insert_agent.php
@@ -1,1 +1,11 @@
+<html>
+	<head><title>Fake Street Realty</title></head>
+	<body>
 
+		<form action="insertagent.php" method=post>
+			Commission Percentage: <input type=number name="commission" size=8> <br><br>
+			<input type=submit name="submit" value="Add Section">
+		</form>
+
+	</body>
+</html>
