6bc98dda342b43c679b109242ea6730c1a17f661
brenthawkings <brenthawk17@gmail.com>
2022-11-08 15:48:54 -0700

    insert_property.php Updated

--- project/insert_property.php
+++ project/insert_property.php
@@ -11,7 +11,7 @@
 	<body>
 		<h2>Add a Property</h2>
 		<form action="insertproperty.php" method=post>
-			Address: <input type=text name="address" size=20><br><br>
+			Address: <input type=text name="address" size=20 required><br><br>
 			Asking Price: <input type=number name="askingprice" size=20><br><br>
 
 			<div = "soldBox">
@@ -22,12 +22,12 @@
 				Sale Price: <input type=number name="saleprice" size=20><br><br>
 			</div>
 
-				Squarefootage: <input type=number name="saleprice" size=20><br><br>
-				Number of Beds: <input type=number name="bedrooms" size=20><br><br>
-				Number of Baths: <input type=number name="bathrooms" size=20><br><br>
-				Date constructed: <input type=date name="constdate" size=20><br><br>
+			Squarefootage: <input type=number name="saleprice" size=20><br><br>
+			Number of Beds: <input type=number name="bedrooms" size=20><br><br>
+			Number of Baths: <input type=number name="bathrooms" size=20><br><br>
+			Date constructed: <input type=date name="constdate" size=20><br><br>
 
-				<input type=submit name="submit" value="Insert"></form>
+			<input type=submit name="submit" value="Insert"></form>
 
 	</body>
 </html>
--- project/main.php
+++ project/main.php
@@ -1,4 +1,11 @@
 <html>
+
+<style>
+body {
+ background-image: url('https://i.kym-cdn.com/entries/icons/original/000/026/008/Screen_Shot_2018-04-25_at_12.24.22_PM.png');
+}
+</style>
+
 <h1> Fake Street Realty </h1>
 <p>Select from one of the following operations:</p>
 <ul>
