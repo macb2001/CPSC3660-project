2b45aca7fc285c806680eff79f588bb30614212a
brenthawkings <brenthawk17@gmail.com>
2022-11-08 15:17:56 -0700

    insert_property.php address asking is sold

--- project/insert_property.php
+++ project/insert_property.php
@@ -1,6 +1,11 @@
 <html>
 	<head><title>Fake Street Realty</title></head>
 	<body>
-
+		<h2>Add a Property</h2>
+		<form action="insertproperty.php" method=post>
+		Address: <input type=text name="address" size=20><br><br>
+		Asking: <input type=number name="askingprice" size=20><br><br>
+		Is sold?: <input type="checkbox" id="sold" name="sold" value="True"><br><br>
+		<input type=submit name="submit" value="Insert"></form>
 	</body>
 </html>
