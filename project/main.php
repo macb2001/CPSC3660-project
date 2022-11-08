ac009b4c0f9ca1c7b19b0dd84231eaca623899b9
photonassassin <56136670+photonassassin@users.noreply.github.com>
2022-11-08 14:30:39 -0700

    Update main.php

--- project/main.php
+++ project/main.php
@@ -1,16 +1,13 @@
 <html>
-<h1> University of Wendy </h1>
+<h1> Fake Street Realty </h1>
 <p>Select from one of the following operations:</p>
 <ul>
-<li><a href="insert_dept.php">Insert a Department</a></li>
-<li><a href="update_dept.php">Update information for a Department</a></li> 
-<li><a href="select_dept.php">List Information on a Department and its courses</a></li> 
-<li><a href="delete_dept.php">Delete a Department</li> 
-<li><a href="insert_course.php">Insert a Course</a></li>
-<li><a href="delete_course.php">Delete a Course</a></li>     
-<li><a href="insert_section.php">Insert a Course Section</a></li>
-<li><a href="select_section.php">List all Sections for a Course</a></li> 
-<li><a href="selectsection2.php">List all Course Sections</a></li> 
+<li><a href="insert_agent.php">Insert a new Agent</a></li>
+<li><a href="select_agent.php">list agents</a></li> 
+<li><a href="insert_client.php">Insert a Course</a></li>
+<li><a href="select_client.php">list clients</a></li> 
+<li><a href="insert_property.php">Insert a Property</a></li>
+<li><a href="select_property.php">List all Sections for a Course</a></li> 
 </ul>
 <p><a href="logout.php">Logout</a></p>
 </html>
