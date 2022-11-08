8aab3d5be51c27fe2d2020d684655dd1155b3d3e
brenthawkings <brenthawk17@gmail.com>
2022-11-08 15:40:14 -0700

    insert_property.php Is sold opens saleprice

--- project/insert_property.php
+++ project/insert_property.php
@@ -1,16 +1,33 @@
 <html>
-	<head><title>Fake Street Realty</title></head>
+	<head>
+		<title>Fake Street Realty</title>
+		<script type = "text/javascript">
+      function promptSold(cbox) {
+        var input = document.getElementById("salepricePrompt");
+        input.style.display = sold.checked ? "block" : "none";
+      }
+    </script>
+	</head>
 	<body>
 		<h2>Add a Property</h2>
 		<form action="insertproperty.php" method=post>
-		Address: <input type=text name="address" size=20><br><br>
-		Asking Price: <input type=number name="askingprice" size=20><br><br>
-		Is sold?: <input type="checkbox" name="sold" value="True"><br><br>
-		Sale Price: <input type=number name="saleprice" size=20><br><br>
-		Squarefootage: <input type=number name="saleprice" size=20><br><br>
-		Number of Beds: <input type=number name="bedrooms" size=20><br><br>
-		Number of Baths: <input type=number name="bathrooms" size=20><br><br>
-		Date constructed: <input type=date name="constdate" size=20><br><br>
-		<input type=submit name="submit" value="Insert"></form>
+			Address: <input type=text name="address" size=20><br><br>
+			Asking Price: <input type=number name="askingprice" size=20><br><br>
+
+			<div = "soldBox">
+				<input type="checkbox" id="sold" name="sold" value="sold" onclick="promptSold(this)">
+					<label for="sold">Sold</label><br>
+			</div><br>
+			<div id = "salepricePrompt" style = "display: none">
+				Sale Price: <input type=number name="saleprice" size=20><br><br>
+			</div>
+
+				Squarefootage: <input type=number name="saleprice" size=20><br><br>
+				Number of Beds: <input type=number name="bedrooms" size=20><br><br>
+				Number of Baths: <input type=number name="bathrooms" size=20><br><br>
+				Date constructed: <input type=date name="constdate" size=20><br><br>
+
+				<input type=submit name="submit" value="Insert"></form>
+
 	</body>
 </html>
