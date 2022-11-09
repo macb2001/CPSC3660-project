81662e6fe39626c51d5f7818e9a3023177594bd3
Mackenzie Broughton <macwbroughton@gmail.com>
2022-11-08 17:19:08 -0700

    fixed add property sale price requirement check

--- project/insert_property.php
+++ project/insert_property.php
@@ -8,18 +8,18 @@
       }
     </script>
 		<script type = "text/javascript">
+			function validateSubmission() {
 				if (document.getElementById("sold").checked) {
 					saleprice.setAttribute("required", "");
 				} else {
 					saleprice.removeAttribute("required");
 				}
-
       }
     </script>
 	</head>
 
 	<body>
-		<h2>Add a Property</h2>
+		<h1>Add a Property</h1>
 		<form action="insertproperty.php" method=post onsubmit="validateSubmission()">
 			Address: <input type=text name="address" size=20 required required><br><br>
 			Asking Price: $<input type=number name="askingprice" size=20 min = "0" required><br><br>
@@ -32,13 +32,13 @@
 				Sale Price: $<input type=number name="saleprice" id = "saleprice" size=20 min = "0"><br><br>
 			</div>
 
-			Squarefootage: <input type=number name="saleprice" size=20 min = "0" required>
+			Squarefootage: <input type=number name="sqrft" size=20 min = "0" required>
 				<label>sq. ft.</label><br><br>
 			Number of Beds: <input type=number name="bedrooms" size=20 min = "0" required><br><br>
 			Number of Baths: <input type=number name="bathrooms" size=20 min = "0" required><br><br>
 			Date constructed: <input type=date name="constdate" size=20 required><br><br>
 
-			<input type=submit name="submit" value="Insert"></form>
+			<input type=submit name="submit" value="Submit"></form>
 
 	</body>
 </html>
