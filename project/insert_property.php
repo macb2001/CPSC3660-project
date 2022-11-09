5d1d6e4629ec04a70937e8c8910183cf32521d34
Pants <aidancampbell123@gmail.com>
2022-11-09 14:23:57 -0700

    added select agent and client in to insert property and added it in to data base

--- project/insert_property.php
+++ project/insert_property.php
@@ -20,7 +20,59 @@
 	<center>
 		<h1>Add a Property</h1>
 		<body>
+
+
 			<form action="insertproperty.php" method=post onsubmit="validateSubmission()">
+
+				<?php
+
+				if (isset($_COOKIE["username"])) {
+					$username = $_COOKIE["username"];
+					$password = $_COOKIE["password"];
+					$conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,$username);
+					if($mysqli->connect_errno) {
+						echo "Connection Issue!";
+						exit;
+					}
+
+				$sql = "SELECT id, name FROM CLIENT, PERSON WHERE id = c_id;";
+				echo "Select a Client <select name='cid' id='cid'>";
+
+				if($conn->query($sql)) {
+					$result = $conn->query($sql);
+					foreach ($result as $data) {
+						echo "<option value='{$data['id']}'>{$data['name']}</option>";
+					}
+					echo "</select><br><br>";
+
+					}
+
+					$sql = "SELECT id, name FROM AGENT, PERSON WHERE id = a_id;";
+					echo "Select a Agent <select name='aid' id='aid'>";
+
+					if($conn->query($sql)) {
+						$result = $conn->query($sql);
+						foreach ($result as $data) {
+							echo "<option value='{$data['id']}'>{$data['name']}</option>";
+						}
+						echo "</select><br><br>";
+
+						}
+
+					else {
+		 	 		$err = $conn->errno;
+		 	 		echo "<p>MySQL error code $err </p>";
+		  	}
+
+			} else {
+		 			echo "<h3>You are not logged in!</h3><p> <a href=\"index.php\">Login First</a></p>";
+		 		}
+					if(isset($conn)) {
+						$conn = null;
+				}
+
+				?>
+
 				Address: <input type=text name="address" size=20 required required><br><br>
 				Asking Price: $<input type=number name="askingprice" size=20 min = "0" required><br><br>
 
--- project/insertproperty.php
+++ project/insertproperty.php
@@ -1,1 +1,50 @@
+<html>
+<head>
+  <title>Inserting Person</title>
+</head>
+<body>
+<?php
+if (isset($_COOKIE["username"])) {
 
+  $cid = $_POST['cid'];
+  $aid = $_POST['aid'];
+  $address = $_POST['address'];
+  $askingprice = $_POST['askingprice'];
+  $sold = $_POST['sold'];
+  $saleprice = $_POST['saleprice'];
+  $sqrft = $_POST['sqrft'];
+  $bedrooms = $_POST['bedrooms'];
+  $bathrooms = $_POST['bathrooms'];
+  $constdate = $_POST['constdate'];
+
+  $username = $_COOKIE["username"];
+  $password = $_COOKIE["password"];
+
+  $conn = new mysqli("vconroy.cs.uleth.ca",$username,$password,$username);
+  if($mysqli->connect_errno) {
+    echo "Connection Issue!";
+    exit;
+  }
+
+  $sql = "INSERT INTO PROPERTY
+    VALUES ('$address', '$aid', '$cid', '$sqrft', '$askingprice', '$sold',
+    '$constdate', '$bedrooms', '$saleprice', '$bthrooms');";
+    if($conn->query($sql)) {
+      $id = $conn->insert_id;
+
+      echo "<p>Record added successfully.\n</p>";
+      echo "<a href='main.php'>back</a>";
+    } else {
+    $err = $conn->errno;
+    echo "<p>MySQL error code $err </p>";
+  }
+} else {
+  echo "<h3>You are not logged in!</h3><p> <a href=\"index.php\">Login First</a></p>";
+}
+
+if(isset($conn)) {
+  $conn = null;
+}
+?>
+</body>
+</html>
