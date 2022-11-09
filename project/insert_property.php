45ae64da38ea2ac6fecdecb3f05fe11f74cb54fd
Mackenzie Broughton <macwbroughton@gmail.com>
2022-11-08 17:01:41 -0700

    added units and min values for some property attributes

--- project/insert_property.php
+++ project/insert_property.php
@@ -13,19 +13,22 @@
 		<h2>Add a Property</h2>
 		<form action="insertproperty.php" method=post>
 			Address: <input type=text name="address" size=20 required><br><br>
-			Asking Price: <input type=number name="askingprice" size=20><br><br>
+			Asking Price: <input type=number name="askingprice" size=20 min = "0">
+				<label>$</label><br><br>
 
 			<div = "soldBox">
 				<input type="checkbox" id="sold" name="sold" value="sold" onclick="promptSold(this)">
 					<label for="sold">Sold</label><br>
 			</div><br>
 			<div id = "salepricePrompt" style = "display: none">
-				Sale Price: <input type=number name="saleprice" size=20><br><br>
+				Sale Price: <input type=number name="saleprice" size=20 min = "0"><br><br>
+					<label>$</label><br><br>
 			</div>
 
-			Squarefootage: <input type=number name="saleprice" size=20><br><br>
-			Number of Beds: <input type=number name="bedrooms" size=20><br><br>
-			Number of Baths: <input type=number name="bathrooms" size=20><br><br>
+			Squarefootage: <input type=number name="saleprice" size=20 min = "0"><br><br>
+				<label>sq. ft.</label><br><br>
+			Number of Beds: <input type=number name="bedrooms" size=20 min = "0"><br><br>
+			Number of Baths: <input type=number name="bathrooms" size=20 min = "0"><br><br>
 			Date constructed: <input type=date name="constdate" size=20><br><br>
 
 			<input type=submit name="submit" value="Insert"></form>
